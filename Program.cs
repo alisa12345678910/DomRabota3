353221a3b7f69499ff032848e72a0bd76b0c69c5
alisa12345678910 <alisa.kumankova@mail.ru>
2022-10-31 23:10:25 +0300

    Exercise 23  solved

--- Program.cs
+++ Program.cs
@@ -15,24 +15,21 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
-Console.WriteLine("Введите координаты x точки а");
-int ax = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите координаты y точки а");
-int ay = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите координаты z точки a");
-int az = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите координаты x точки b");
-int bx = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите координаты y точки b");
-int by = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите координаты z точки b");
-int bz = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координаты x точки а");
+// int ax = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координаты y точки а");
+// int ay = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координаты z точки a");
+// int az = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координаты x точки b");
+// int bx = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координаты y точки b");
+// int by = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координаты z точки b");
+// int bz = Convert.ToInt32(Console.ReadLine());
 
-double dist = Math.Sqrt(Math.Pow(bx-ax, 2) + Math.Pow(by - ay, 2) + Math.Pow(bz - az, 2));
-Console.WriteLine($"Расстояние между двумя точками равно {dist}");
-
-
-
+// double dist = Math.Sqrt(Math.Pow(bx-ax, 2) + Math.Pow(by - ay, 2) + Math.Pow(bz - az, 2));
+// Console.WriteLine($"Расстояние между двумя точками равно {dist}");
 
 
 // Задача 23
@@ -42,3 +39,9 @@ Console.WriteLine($"Расстояние между двумя точками р
 
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число n");
+int n=Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+Console.Write($"{Math.Pow(i, 3)}, ");
