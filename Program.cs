dbd11d730acd75f7194fbd86b0693b0380b222e2
alisa12345678910 <alisa.kumankova@mail.ru>
2022-10-31 22:56:54 +0300

    Exercise 21 solved

--- Program.cs
+++ Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 19
+
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом. Через строку решать нельзя.
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+// Задача 21
+
+// Напишите программу, которая принимает на вход координаты двух точек и 
+// находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Введите координаты x точки а");
+int ax = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты y точки а");
+int ay = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты z точки a");
+int az = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты x точки b");
+int bx = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты y точки b");
+int by = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты z точки b");
+int bz = Convert.ToInt32(Console.ReadLine());
+
+double dist = Math.Sqrt(Math.Pow(bx-ax, 2) + Math.Pow(by - ay, 2) + Math.Pow(bz - az, 2));
+Console.WriteLine($"Расстояние между двумя точками равно {dist}");
+
+
+
+
+
+// Задача 23
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу
+//  кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
